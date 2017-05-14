4a6be6df5ee84ae078a88b7c70757b6392997cbd
Ruben Boumans <ruben.boumans@debijenkorf.nl>
2017-05-14 10:51:32 +0200

    Integrate

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_from_directory
 import smtplib
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.utils import formatdate
 import sys
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/recording")
+def make():
+    return render_template('recording.html')
 
 @app.route("/send", methods=['POST'])
 def send():
@@ -32,16 +37,19 @@ def send():
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.ehlo()
     server.starttls()
-    server.login("appreciation10x@gmail.com", "secret")
+    server.login("appreciation10x@gmail.com", os.environ.get('PASSWORD', ""))
     server.sendmail(sender, receiver, message.as_string())
     server.close()
 
-    return redirect("success")
+    return redirect("share")
 
+@app.route('/static/<path:path>')
+def get_static(path):
+    return send_from_directory('static', path)
 
-@app.route("/success")
-def success():
-    return render_template('success.html')
+@app.route("/share")
+def share():
+    return render_template('share.html')
 
 if __name__ == '__main__':
      app.run(debug=True, host="0.0.0.0", port=int(sys.argv[1]))
